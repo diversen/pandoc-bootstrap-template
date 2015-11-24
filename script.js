3306e6d4cefd8ca7a99939053d762144094f222b
Dennis Iversen <dennis.iversen@gmail.com>
2015-11-24 16:19:21 +0100

    sticky menu. Accordian.

---
+++ script.js
@@ -0,0 +1,67 @@
+
+$(document).ready(function($){
+    $(".well ul").addClass('menu'); 
+    $(".menu").attr('id', 'accordion-1');
+});
+
+$(document).ready(function($){
+    $("#TOC").stick_in_parent();
+});
+
+$(document).ready(function($){
+					$('#accordion-1').dcAccordion({
+						eventType: 'click',
+						autoClose: true,
+						saveState: true,
+						disableLink: false,
+						speed: 'slow',
+						showCount: true,
+						autoExpand: true,
+						cookie	: 'dcjq-accordion-1',
+						classExpand	 : 'dcjq-current-parent'
+					});
+					$('#accordion-2').dcAccordion({
+						eventType: 'click',
+						autoClose: false,
+						saveState: true,
+						disableLink: true,
+						speed: 'fast',
+						classActive: 'test',
+						showCount: true
+					});
+					$('#accordion-3').dcAccordion({
+						eventType: 'click',
+						autoClose: false,
+						saveState: false,
+						disableLink: false,
+						showCount: false,
+						speed: 'slow'
+					});
+					$('#accordion-4').dcAccordion({
+						eventType: 'hover',
+						autoClose: true,
+						saveState: true,
+						disableLink: true,
+						menuClose: false,
+						speed: 'slow',
+						showCount: true
+					});
+					$('#accordion-5').dcAccordion({
+						eventType: 'hover',
+						autoClose: false,
+						saveState: true,
+						disableLink: true,
+						menuClose: true,
+						speed: 'fast',
+						showCount: true
+					});
+					$('#accordian-6').dcAccordion({
+						eventType: 'hover',
+						autoClose: false,
+						saveState: false,
+						disableLink: false,
+						showCount: false,
+						menuClose: true,
+						speed: 'slow'
+					});
+});
